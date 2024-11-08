4480164c175764212712704d9a164a06998cc48f
ArenaMirror03 <officialsumitsharma03@gmail.com>
2024-11-09 01:03:29 +0530

    reverse programming

--- user_name.c
+++ user_name.c
@@ -2,26 +2,22 @@
 #include <string.h>
 
 int main() {
-    char name[100];
-    int length, i;
+  char name[10];
+  int length, i;
 
-   
-    printf("Enter your name: ");
-    fgets(name, sizeof(name), stdin);  
+  printf("Enter your name: ");
+  fgets(name, sizeof(name), stdin);
 
+  name[strcspn(name, "\n")] = '\0';
 
-    name[strcspn(name, "\n")] = '\0';
+  length = strlen(name);
 
-   
-    length = strlen(name);
+  printf("Reversed name: ");
+  for (i = length - 1; i >= 0; i--) {
+    printf("%c", name[i]);
+  }
 
-    
-    printf("Reversed name: ");
-    for (i = length - 1; i >= 0; i--) {
-        printf("%c", name[i]);
-    }
+  
 
-    printf("\n");
-
-    return 0;
+  return 0;
 }
