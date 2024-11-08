4cdfd0d8b1d99b29288a01b74a0c9f15c84d0d6d
ArenaMirror03 <officialsumitsharma03@gmail.com>
2024-11-09 00:34:29 +0530

    test program

--- test.c
+++ test.c
@@ -1,4 +1,12 @@
 #include<stdio.h>
 int main(){
-    printf("hello world");
+    int a,b,c;
+    printf("enter any number: \n");
+    scanf("%d", &a);
+    printf("enter any number: \n");
+    scanf("%d", &b);
+    printf("enter any number: \n");
+    scanf("%d", &c);
+    printf("the sum is %d", a+b+c);
+    return 0;
 }
